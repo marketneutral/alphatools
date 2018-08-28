0f40e5009f990b4f160f1069d8b2d0b7c7d555a2
marketneutral <jonathan.r.larkin@gmail.com>
2018-08-28 12:33:47 -0400

    Update setup for PyPI release

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
 setup(
-   name='alphatools',
-   version='0.1',
-   description='Quant finance resarch tools',
-   author='Jonathan Larkin',
-   author_email='jonathan.r.larkin@gmail.com',
-   packages=['alphatools',
-             'alphatools.ics',
-             'alphatools.fundamentals'],
-   install_requires=['numpy', 'pandas', 'zipline'],
+    name='alphatools',
+    version='0.1',
+    description='Quant finance resarch tools',
+    author='Jonathan Larkin',
+    author_email='jonathan.r.larkin@gmail.com',
+    url = "https://github.com/marketneutral/alphatools",
+    download_url = "https://github.com/marketneutral/alphatools/archive/0.10.tar.gz",
+    packages=['alphatools',
+              'alphatools.ics',
+              'alphatools.fundamentals'],
+    install_requires=['numpy', 'pandas', 'zipline'],
 )
