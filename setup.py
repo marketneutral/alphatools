4a86da6a2a77b6e93553b24642668e509694d0c3
marketneutral <jonathan.r.larkin@gmail.com>
2018-08-28 21:50:01 -0400

    Update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='alphatools',
-    version='0.1',
+    version='0.11',
     description='Quant finance resarch tools',
     author='Jonathan Larkin',
     author_email='jonathan.r.larkin@gmail.com',
     url = "https://github.com/marketneutral/alphatools",
-    download_url = "https://github.com/marketneutral/alphatools/archive/0.10.tar.gz",
+    download_url = "https://github.com/marketneutral/alphatools/archive/0.11.tar.gz",
     packages=['alphatools',
               'alphatools.ics',
               'alphatools.fundamentals'
