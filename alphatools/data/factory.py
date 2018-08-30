48226369def8aef45a833d01eadc1156fd7d127d
marketneutral <jonathan.r.larkin@gmail.com>
2018-08-30 11:09:06 -0400

    Add pipeline data Factory

---
+++ alphatools/data/factory.py
@@ -0,0 +1,35 @@
+import blaze as bz
+import json
+from datashape import dshape
+
+from alphatools.research import loaders, blaze_loader
+from zipline.data import bundles
+from zipline.utils.calendars import get_calendar
+from zipline.pipeline.loaders.blaze import from_blaze
+
+from os import path
+
+this_file = path.dirname(__file__)
+
+with open(path.join(this_file, 'factory/data_sources.json')) as f:
+    data_sources = json.load(f)
+
+Factory = {}
+
+for source in data_sources.keys():
+    loc = data_sources[source]['url']
+    shape = dshape(data_sources[source]['schema'])
+
+    expr = bz.data(
+        loc,
+        dshape=shape
+    )
+    
+    # create the DataSet
+    ds = from_blaze(
+        expr,
+        no_deltas_rule='ignore',
+        no_checkpoints_rule='ignore',
+        loader=blaze_loader
+    )
+    Factory = {source: ds}
