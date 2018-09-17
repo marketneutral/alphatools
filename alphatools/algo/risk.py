c65c063bcc2f916e58514a16360e9aeaff1717d8
marketneutral <jonathan.r.larkin@gmail.com>
2018-09-17 19:11:22 -0400

    Add cvar; docstrings

--- alphatools/algo/risk.py
+++ alphatools/algo/risk.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 
 def L1_risk(wgts, returns):
+    """
+    Returns the "L1" risk as per Konno & Yamazaki; aka the mean-absolute-
+    deviation.
+    """
     rets = returns.fillna(0.0).as_matrix()
     return np.mean(np.abs(rets.dot(wgts)))
 
@@ -9,10 +13,25 @@ def value_at_risk(
         weights,
         returns,
         alpha=0.95):
-    
+    """
+    Returns the historical simulation VaR at the confidence threshold.
+    """
     returns = returns.fillna(0.0).as_matrix()
     portfolio_returns = returns.dot(weights)
     return np.percentile(portfolio_returns, 100 * (1-alpha))
+
+def expected_shortfall(
+        weights,
+        returns,
+        alpha=0.95):
+    """
+    Returns the historical simulation CVaR at the confidence threshold.
+    """
+
+    var = value_at_risk(weights, returns, alpha)
+    returns = returns.fillna(0.0)
+    portfolio_returns = returns.dot(weights)
+    return np.nanmean(portfolio_returns[portfolio_returns < var])
 
 
 def calc_portfolio_risk(
@@ -21,7 +40,15 @@ def calc_portfolio_risk(
         risk_func,
         hist_days=180,
         **kwargs):
+    """
+    This is a helper function designed to be the primary call in an algo
+    for calculating portfolio-level risk. It takes the current context and
+    data objects and a `risk_func` (e.g., `value_at_risk`), and formats 
+    portfolio weights and returns such that the indicies line up in a numpy
+    array.
+    """
 
+    
     positions = context.portfolio.positions
     positions_index = pd.Index(positions)
     share_counts = pd.Series(  
