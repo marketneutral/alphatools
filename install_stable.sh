52962855b480a78cb3bbdd7af100c9b4ef0fac27
marketneutral <jonathan.r.larkin@gmail.com>
2018-10-04 17:03:02 -0400

    Update install stable

--- install_stable.sh
+++ install_stable.sh
@@ -5,7 +5,7 @@ python -m pip install -r requirements_blaze_stable.txt --no-cache-dir
 pip install zipline==1.3.0 --no-cache-dir
 pip install ipykernel --no-cache-dir
 pip install alphalens==0.3.2 --no-cache-dir
-pip install pyfolio --no-cache-dir   # brings sklearn too
+pip install pyfolio --no-cache-dir
 export MKL_THREADING_LAYER=GNU       # required for using mkl-service
 cd ..
 pip install -e alphatools --no-cache-dir
---
+++ test_pymc3.py
@@ -0,0 +1,46 @@
+import pymc3 as pm
+
+if __name__=='__main__':
+    
+    print('Running on PyMC3 v{}'.format(pm.__version__))
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # Initialize random number generator
+    np.random.seed(123)
+
+    # True parameter values
+    alpha, sigma = 1, 1
+    beta = [1, 2.5]
+
+    # Size of dataset
+    size = 100
+
+    # Predictor variable
+    X1 = np.random.randn(size)
+    X2 = np.random.randn(size) * 0.2
+    
+    # Simulate outcome variable
+    Y = alpha + beta[0]*X1 + beta[1]*X2 + np.random.randn(size)*sigma
+    
+    basic_model = pm.Model()
+
+    with basic_model:
+
+        # Priors for unknown model parameters
+        alpha = pm.Normal('alpha', mu=0, sd=10)
+        beta = pm.Normal('beta', mu=0, sd=10, shape=2)
+        sigma = pm.HalfNormal('sigma', sd=1)
+        
+        # Expected value of outcome
+        mu = alpha + beta[0]*X1 + beta[1]*X2
+        
+        # Likelihood (sampling distribution) of observations
+        Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=Y)
+
+    with basic_model:
+        # draw 500 posterior samples
+        trace = pm.sample(500)
+
+    print(trace['alpha'][-5:])
