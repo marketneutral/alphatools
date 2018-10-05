30bb5f4bf49bafff40f1f7f181b535be9e017d4e
marketneutral <jonathan.r.larkin@gmail.com>
2018-10-04 20:46:41 -0400

    Update install stable script

--- install_stable.sh
+++ install_stable.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 conda create -n env_alphatools_stable -y python=3.5 numpy=1.11.3 pandas=0.18.1 scipy=0.17.1 libgfortran=3.0 mkl-service pymc3=3.1 scikit-learn pip
 source activate env_alphatools_stable
 python -m pip install -r requirements_stable.txt --no-cache-dir
@@ -10,7 +12,7 @@ cd ..
 pip install -e alphatools --no-cache-dir
 python -m ipykernel install --user --name env_alphatools_stable --display-name "Python 3.5 (env_alphatools_stable)"
 
-source ~/.basrc
+source ~/.bashrc
 # must append to .bashrc
 if [ "$MKL_THREADING_LAYER" = "" ]
 then
