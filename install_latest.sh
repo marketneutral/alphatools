a6a1d1d94c27148f04a4dad4fe230832063841b6
marketneutral <jonathan.r.larkin@gmail.com>
2018-10-07 16:55:02 -0400

    Prep for Python 3.6

--- install_latest.sh
+++ install_latest.sh
@@ -1,4 +1,6 @@
-conda create -n env_alphatools_latest -y python=3.5 numpy=1.14.1 pandas=0.22.0 scipy=1.0.0 libgfortran=3.0 pip
+#!/usr/bin/env bash
+
+conda create -n env_alphatools_latest -y python=$PYTHON_VERSION numpy=1.14.1 pandas=0.22.0 scipy=1.0.0 libgfortran=3.0 pip
 source activate env_alphatools_latest
 python -m pip install -r requirements_latest.txt --no-cache-dir
 python -m pip install -r requirements_blaze_latest.txt --no-cache-dir
@@ -7,4 +9,4 @@ pip install statsmodels==0.9.0 --upgrade --no-cache-dir
 pip install ipykernel --no-cache-dir
 cd ..
 pip install -e alphatools --no-cache-dir
-python -m ipykernel install --user --name env_alphatools_latest --display-name "Python 3.5 (env_alphatools_latest)"
+python -m ipykernel install --user --name env_alphatools_latest --display-name "env_alphatools_latest"
