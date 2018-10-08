6936edd59d35f81e3298b2952ed0f7eb3b4894de
marketneutral <jonathan.r.larkin@gmail.com>
2018-10-08 10:15:18 -0400

    fix README, bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alphatools',
-    version='0.11',
+    version='0.15',
     description='Quant finance resarch tools',
     author='Jonathan Larkin',
     author_email='jonathan.r.larkin@gmail.com',
